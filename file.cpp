1cd904366dce4525a44747b32ab2cdfc974b3c8f
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-08-31 08:25:06 -0700

    trial

---
+++ file.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int input;
+  cin >> input;
+  cout << "You entered:" << input << endl;
+  input = input + 5;
+  cout << input;
+  return 0;
+}
