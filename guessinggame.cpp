daf601f603865547e3236e92e1a628789a79465d
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-05 08:44:59 -0700

    guessing game working and commented

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -10,17 +10,17 @@ int main()
    bool winner = false;
    int guesscount = 0;
   cout << "Welcome to the Guessing Game! Guess any number between 0 and 100 (inclusive)" << endl;
-  while (winner != true) {
+  while (winner != true) { //while loop that uses a boolean and once the user guesses the right answer the boolean turns true and ends the loop
      cin >> guess;
-     if (guess > answer) {
+     if (guess > answer) { //if the guess is higher in the answer it tells the user that and adds one to guess counter
         cout << "You guessed " << guess << " the answer is lower" << endl;
         guesscount += 1;
      }
-     if (guess < answer) {
+     if (guess < answer) { //if the guess is lower than the answer it tell the user that and adds one to the guess counter
         cout << "You guessed " << guess << " the answer is higher" << endl;
         guesscount += 1;
      }
-     if (guess == answer) {
+     if (guess == answer) { //when the guess equals the answer it tells the user they are correct and stops the while loop
         cout << "You are correct!! " << endl;
         winner = true;
         guesscount += 1;
