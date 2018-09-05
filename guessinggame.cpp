c2d45dc4335b947f03775b78edafaac6e606b4ff
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-05 08:36:21 -0700

    guessing game

---
+++ guessinggame.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+  {
+   int answer = rand() % 100 + 1;
+   int guess;
+   bool winner = false;
+   int guesscount = 0;
+  cout << "Welcome to the Guessing Game! Guess any number between 0 and 100 (inclusive)" << endl;
+  while (winner != true) {
+     cin >> guess;
+     if (guess > answer) {
+        cout << "You guessed " << guess << "the answer is lower" << endl;
+        guesscount += 1;
+     }
+     if (guess < answer) {
+        cout << "You guessed " << guess << "the answer is higher" << endl;
+        guesscount += 1;
+     }
+     if (guess == answer) {
+        cout << "You are correct!! " << endl;
+        winner = true;
+        guesscount += 1;
+     }
+  }
+  cout << "It took you " << guesscount << "tries!!!" << endl;
+  return 0;
+ }
