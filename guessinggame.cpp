3f6c25cdb0725c35836a3499f5a66b633fe7f221
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-13 09:04:22 -0700

    Final GuessingGame

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -3,29 +3,46 @@
 #include <cstdlib>
 using namespace std;
 
-int main()
+void playgame();
+
+int main() {
+  playgame();
+  return 0;
+}
+
+void playgame()
   {
+    srand (time(NULL));
    int answer = rand() % 100 + 1;
    int guess;
    bool winner = false;
    int guesscount = 0;
-  cout << "Welcome to the Guessing Game! Guess any number between 0 and 100 (inclusive)" << endl;
-  while (winner != true) { //while loop that uses a boolean and once the user guesses the right answer the boolean turns true and ends the loop
-     cin >> guess;
-     if (guess > answer) { //if the guess is higher in the answer it tells the user that and adds one to guess counter
-        cout << "You guessed " << guess << " the answer is lower" << endl;
-        guesscount += 1;
+
+     cout << "Welcome to the Guessing Game! Guess any number between 0 and 100 (inclusive)" << endl;
+     while (winner != true) { //while loop that uses a boolean and once the user guesses the right answer the boolean turns true and ends the loop
+       cin >> guess;
+       if (guess > answer) { //if the guess is higher in the answer it tells the user that and adds one to guess counter
+	 cout << "You guessed " << guess << " the answer is lower" << endl;
+	 guesscount += 1;
+       }
+       if (guess < answer) { //if the guess is lower than the answer it tell the user that and adds one to the guess counter
+	 cout << "You guessed " << guess << " the answer is higher" << endl;
+	 guesscount += 1;
+       }
+       if (guess == answer) { //when the guess equals the answer it tells the user they are correct and stops the while loop
+	 cout << "You are correct!! " << endl;
+	 winner = true;
+	 guesscount += 1;
+       }
      }
-     if (guess < answer) { //if the guess is lower than the answer it tell the user that and adds one to the guess counter
-        cout << "You guessed " << guess << " the answer is higher" << endl;
-        guesscount += 1;
-     }
-     if (guess == answer) { //when the guess equals the answer it tells the user they are correct and stops the while loop
-        cout << "You are correct!! " << endl;
-        winner = true;
-        guesscount += 1;
-     }
+   
+  cout << "It took you " << guesscount << " tries!!!" << endl;
+  cout << "Would you like to play again? If yes type 'y' if no type 'n'" << endl;
+  char play;
+  cin >> play;
+  if (play == 'y') {
+    playgame();
   }
-  cout << "It took you " << guesscount << "tries!!!" << endl;
-  return 0;
+  
+  return;
   }
