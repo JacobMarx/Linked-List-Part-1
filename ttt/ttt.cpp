062d97ead3afcdee07cb2e1405aaf24069070349
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-25 09:13:47 -0700

    finished print working on moves function

--- ttt/ttt.cpp
+++ ttt/ttt.cpp
@@ -1,43 +1,81 @@
-/*
+/*xc
  Jacob Marx, 9 September 2018
  This program is a tic tac toe game
  */
 #include <iostream>
 
 using namespace std;
-//void moves;
-char printBoard;
+void moves(char array[][3]);
+void printBoard(char print[][3]);
 //void checkWin;
+int move = 1;
 
 int main() {
   char board[3][3];
-  //moves();
-  printBoard(&board);
+  printBoard(board);
+  moves(&board);
   //checkWin();
 }
 
-//void moves() {
- 
-//return;
-//}
+void moves(char &get[][3]) {
+  char x;
+  char y;
 
-char printBoard(char print[3][3]) {
-  char secOne[3];
-  char secTwo[3];
-  char secThree[3];
+  if (move == 1) {
+    cout << "it is x's turn enter fsomejshltguthslfd" << endl;
+  }
+  if (move == 2) {
+    cout << "it is o's tuen enter something" << endl;
+  }
+  
+  count << "enter your column" << endl;
+  cin.get(x, 1);
+  cin.clear();
+  cin.ignore(10000, 0x00);
+  count << "enter the row" << endl;
+  cin.get(y, 1);
+  cin.clear();
+  cin.ignore(10000, 0x00);
+
+  if (move == 1) {
+    get[x][d-y] = 'X';
+    move++;
+  }
+  if (move == 2) {
+    get[x][d-y] = 'X';
+    move--;
+  }
+  
+ return;
+}
+
+void printBoard(char print[][3]) {
+  char secOne[4];
+  char secTwo[4];
+  char secThree[4];
 
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      print[i][j] = 1;
+      print[i][j] = 0x00;
     }
   }
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      print[i][j] = 'x';
+    }
+  }
+  
   for (int i = 0; i < 3 ; i++) {
     secOne[i] = print[0][i];
     secTwo[i] = print[1][i];
     secThree[i] = print[2][i];
+    secOne[3] = '\0';
+    secTwo[3] = '\0';
+    secThree[3] = '\0';
   }
 
-  cout << "  1 2 3" << endl;
+  cout << "  123" << endl;
   cout << "a " << secOne << endl << "b " << secTwo << endl << "c " << secThree << endl;  
 
   return;
