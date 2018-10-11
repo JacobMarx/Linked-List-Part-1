1d806c60231ef131cf7ad3fd98aa5d1d7115d4c0
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-10-11 09:15:02 -0700

    Everything done but delete just need to erase the struct

--- student/student.cpp
+++ student/student.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <math.h>
+
 
 using namespace std;
 
@@ -11,28 +13,33 @@ struct Student {
   float gpa;
 };
 
-void ADDStudent(vector <Student*>* list);
-void PRINTStudent(vector <Student*>* list);
+void DELETEStudent(vector<Student*>* list);
+void ADDStudent(vector<Student*>* list);
+void PRINTStudent(vector<Student*>* list);
 
 bool running = true;
 
 int main() {
+
+  vector<Student*>* vect = new vector<Student*>();
  
   while (running == true) {
     char input[7];
-    input[6] = '\0';
-    vector <Student*>* vect = new vector<Student*>();
     cout << "Do you want to ADD, DELETE, or PPRINT students type your answer. (e.g. 'ADD')" << endl;
-    cin.get(input, 6);
-    cin.get();
+    cin.get(input, 7);
+    cin.clear();
+    cin.ignore(10000, '\n');
     if (strcmp(input, "ADD") == 0) {
-    ADDStudent(vect);
+      ADDStudent(vect);
+      cout << vect -> size() << endl;
     }
-    if (strcmp(input,"DELETE") == 0) {
-
+    //cout << strcmp(input, "DELETE") << endl;
+    //cout << input << endl;
+    if (strcmp(input, "DELETE") == 0) {
+      DELETEStudent(vect);  
     }
-    if (strcmp(input,"PRINT") == 0) {
-    PRINTStudent(vect);
+    if (strcmp(input, "PRINT") == 0) {
+      PRINTStudent(vect);
     }
   }
 }
@@ -48,15 +55,54 @@ void ADDStudent(vector <Student*>* list) {
   cout << "What is the student's gpa" << endl;
   cin >> newStudent -> gpa;
   list -> push_back(newStudent);
+  cin.clear();
+  cin.ignore(10000, '\n');
   return;
 }
 
-void PRINTStudent(vector <Student*>* list) {
-  for (vector <Student*>:: iterator it = list->begin() ; it != list->end(); ++it) {
-
-    cout << (*it) -> firstN << endl;
-
+void PRINTStudent(vector<Student*>* list) {
+  int student = 1;
+  for (vector<Student*>::iterator it = list -> begin(); it != list -> end(); ++it) {
+    /*
+    cout << endl;
+    cout << "Student: " << student << endl;
+    cout << "First Name: " << (*it) -> firstN << endl;
+    cout << "Last Name: " << (*it) -> lastN << endl;
+    cout << "ID: " << (*it) -> id << endl;
+    cout << "GPA: " << (*it) -> gpa << endl;
+    student++;
+    */
+    cout << endl;
+    cout << (*it) -> firstN << " " << (*it) -> lastN << ", " << (*it) -> id << ", "
+	 << (*it) -> gpa  << endl;
+    cout << endl;
   }
+  cout << "hi" << endl;
+  cout << list -> size() << endl;
+}
 
+void DELETEStudent(vector<Student*>* list) {
+  //cout << "hello" << endl;
+  char idint[7];
+  cout << "Enter the ID of the student you wish to delete." << endl;
+  cin.get(idint, 7);
+  cin.ignore(10000, '\n');
+  //cout << idint << endl;
+  int idcomp = 0;
+  for (int i = 0; i < strlen(idint); i++) {
+    //cout << idint[i] << endl;
+    int goon = pow(10, strlen(idint) - i - 1);
+    //cout << goon << endl;
+    idcomp += (idint[i] - '0') * goon;
+    //cout << idcomp << endl;
+  }
+  cout << idcomp << endl;
+  for (vector<Student*>::iterator it = list -> begin(); it != list -> end(); ++it) {
+    if ((*it) -> id - idcomp == 0) {
+      list -> erase (*it);
+    cout << "hi" << endl;
+    }
+  }
+  
 }
 
