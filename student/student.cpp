fbeee0d2e8f724226a14112583ec9925865b118c
U-BSD\331733 <331733@SHS-10L74C2E7.beaverton.k12.or.us>
2018-10-15 09:12:59 -0700

    Everything done verifying inputs

--- student/student.cpp
+++ student/student.cpp
@@ -16,6 +16,7 @@ struct Student {
 void DELETEStudent(vector<Student*>* list);
 void ADDStudent(vector<Student*>* list);
 void PRINTStudent(vector<Student*>* list);
+void commands();
 
 bool running = true;
 
@@ -24,9 +25,9 @@ int main() {
   vector<Student*>* vect = new vector<Student*>();
  
   while (running == true) {
-    char input[7];
-    cout << "Do you want to ADD, DELETE, or PPRINT students type your answer. (e.g. 'ADD')" << endl;
-    cin.get(input, 7);
+    char input[10];
+    cout << "Do you want to ADD, DELETE, or PPRINT students type your answer. For help type 'COMMANDS'." << endl;
+    cin.get(input, 10);
     cin.clear();
     cin.ignore(10000, '\n');
     if (strcmp(input, "ADD") == 0) {
@@ -41,17 +42,51 @@ int main() {
     if (strcmp(input, "PRINT") == 0) {
       PRINTStudent(vect);
     }
+    if (strcmp(input, "QUIT") == 0) {
+      break;
+    }
+    if (strcmp(input, "COMMANDS") == 0) {
+      commands();
+    }
   }
 }
 
 void ADDStudent(vector <Student*>* list) {
   Student* newStudent = new Student();
+  char input[100000];
+  int count = 6;
+  int intcon = 0;
   cout << "What is the students first name?" << endl;
   cin >> newStudent -> firstN;
   cout << "What is the student's last name?" << endl;
   cin >> newStudent -> lastN;
+  cin.clear();
+  cin.ignore(1000, '\n');
   cout << "Enter the studen'ts id number. Maximum of 6 characters." << endl;
-  cin >> newStudent -> id;
+  cin.get(input, 10000);
+  cin.clear();
+  cin.ignore(1000, '\n');
+  if (strlen(input) > 7) {
+    cout << "Input invalid, the id must be only 6 integers." << endl;
+  }
+  for (int i = 0; i < 6; i++) {
+    if (input[i] <= 57 && input[i] >= 47) {
+      count--;
+    }
+    if (input[i] > 57 || input [i] < 47) {
+      cout << "Input invalid, the id must be only 6 integers." << endl;
+    }
+  }
+  for (int i = 0; i < strlen(input); i++) {
+    //cout << idint[i] << endl;
+    int goon = pow(10, strlen(input) - i - 1);
+    //cout << goon << endl;
+    intcon += (input[i] - '0') * goon;
+    //cout << idcomp << endl;
+  }
+  if (count == 0) {
+    newStudent -> id = intcon;
+  }
   cout << "What is the student's gpa" << endl;
   cin >> newStudent -> gpa;
   list -> push_back(newStudent);
@@ -77,7 +112,7 @@ void PRINTStudent(vector<Student*>* list) {
 	 << (*it) -> gpa  << endl;
     cout << endl;
   }
-  cout << "hi" << endl;
+  //cout << "hi" << endl;
   cout << list -> size() << endl;
 }
 
@@ -99,10 +134,22 @@ void DELETEStudent(vector<Student*>* list) {
   cout << idcomp << endl;
   for (vector<Student*>::iterator it = list -> begin(); it != list -> end(); ++it) {
     if ((*it) -> id - idcomp == 0) {
-      list -> erase (*it);
-    cout << "hi" << endl;
+      delete *it;
+      list -> erase (it);
+      break;
+      //cout << "hi" << endl;
     }
   }
+  
+}
+
+void commands() {
+
+  cout << "-----------> COMMANDS <-----------" << endl << endl << "'ADD' = Adds a student to the list with given information." << endl <<
+    "'PRINT' = Print all students in the list to the console." << endl << "'DELETE' = Deletes the student from the list with their specified id." <<
+    endl << "'QUIT' = Exits the program." << endl << endl << "----------------------------------" << endl;
+
+  return;
   
 }
 
